c680c1125ca634d8f7408eab9e649b07b4fd34e9
Daniil <93822098+Daniil8k@users.noreply.github.com>
2022-08-17 20:06:17 +0300

    heroku port

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const auth = require('json-server-auth')
 
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
+const port = process.env.PORT || 3000
 
 // /!\ Bind the router db to the app
 app.db = router.db
@@ -10,4 +11,4 @@ app.db = router.db
 // You must apply the auth middleware before the router
 app.use(auth)
 app.use(router)
-app.listen(3000)
+app.listen(port)
